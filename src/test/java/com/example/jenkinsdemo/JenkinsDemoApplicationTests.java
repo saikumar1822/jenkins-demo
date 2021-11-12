8a58f30dad646fff28967343a237376eaf01651f
saikumar1822 <saikumaryaddalapoori1822@gmail.com>
2021-11-12 10:40:32 +0530

    second commit

--- src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/example/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.jenkinsdemo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JenkinsDemoApplicationTests {
-
+	static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("test executing*******************");
 	}
 
 }
