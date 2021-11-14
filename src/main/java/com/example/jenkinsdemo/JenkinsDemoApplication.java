23f175716ccd94f5879a4ed5bab1607a62972144
saikumar1822 <saikumaryaddalapoori1822@gmail.com>
2021-11-14 16:56:22 +0530

    final commit

--- src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/example/jenkinsdemo/JenkinsDemoApplication.java
@@ -4,10 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-public class JenkinsDemoApplication {
+public class JenkinsDemoApplication extends SpringBootServletInitializer{
 	  static Logger logger = LoggerFactory.getLogger(JenkinsDemoApplication.class);
 	public static void main(String[] args) {
 		logger.info("********************* IN main class********************");
